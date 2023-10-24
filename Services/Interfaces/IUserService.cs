dc6b7de471d906091c1ec42ff7d30aefa37b4bd4
Achiles <achiles1707@gmail.com>
2023-10-23 22:47:36 -0300

    Commit

---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using IbgeApi.Repository.Interfaces;
+using IbgeApi.ViewModels;
+
+namespace IbgeApi.Services.Interfaces;
+
+public interface IUserService
+{
+    Task<IResult> Signup(CreateUserViewModel model);
+    Task<IResult> Login(LoginViewModel model);
+}
