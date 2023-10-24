53b3556090d62044de634f416af214105092a4ed
Achiles <achiles1707@gmail.com>
2023-10-23 22:43:15 -0300

    change: Added UserService  and fixed handler name

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using IbgeApi.Models;
 using IbgeApi.Repository;
 using IbgeApi.Repository.Interfaces;
 using IbgeApi.Services;
+using IbgeApi.Services.Interfaces;
 using IbgeApi.ViewModels;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,12 +15,13 @@ builder.Services.AddDbContext<IbgeDbContext>(options =>
     options.UseSqlite(connectionString));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<TokenService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
 app.MapGet("/", ApiHealthCheckHandler.Get);
-app.MapPost("/signin", AccountHandler.SignIn);
+app.MapPost("/signup", AccountHandler.SignUp);
 app.MapPost("/login", AccountHandler.Login);
 
 app.Run();
