9a7429e59203d37bc3dedbc846472b78ae6cff09
Achiles <achiles1707@gmail.com>
2023-10-26 18:04:15 -0300

    Add: Swagger config and more endpoints

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 using IbgeApi.ApiHandlers;
 using IbgeApi.Data;
-using IbgeApi.Models;
 using IbgeApi.Repository;
 using IbgeApi.Repository.Interfaces;
 using IbgeApi.Services;
 using IbgeApi.Services.Interfaces;
-using IbgeApi.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using ILocationService = IbgeApi.Services.Interfaces.ILocationService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,14 +13,47 @@ var connectionString = builder.Configuration.GetConnectionString("SQLite");
 builder.Services.AddDbContext<IbgeDbContext>(options =>
     options.UseSqlite(connectionString));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ILocationRepository, LocationRepository>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ILocationService, LocationService>();
+
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+#region Api Heath Check
+
 app.MapGet("/", HealthCheckHandler.Get);
+
+#endregion
+
+#region User
+
 app.MapPost("/signup", AccountHandler.SignUp);
 app.MapPost("/login", AccountHandler.Login);
+
+#endregion
+
+#region Location
+
+app.MapPost("/location", LocationHandler.Create);
+app.MapGet("/locations/{skip}/{take}", LocationHandler.GetAll);
+app.MapGet("/location/{code}", LocationHandler.GetByIbgeCode);
+app.MapGet("/location/state/{state}", LocationHandler.GetByState);
+app.MapGet("/location/city/{city}", LocationHandler.GetByCity);
+app.MapPut("/location/{code}", LocationHandler.Update);
+app.MapDelete("/location/{code}", LocationHandler.Remove);
+
+#endregion
 
 app.Run();
