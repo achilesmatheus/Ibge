22862a28135eafba9b58fe0ba9c7c11a90691145
Achiles <achiles1707@gmail.com>
2023-10-26 20:13:15 -0300

    Add: Login method

--- ApiHandlers/AccountHandler.cs
+++ ApiHandlers/AccountHandler.cs
@@ -3,8 +3,8 @@ using IbgeApi.Repository;
 using IbgeApi.Repository.Interfaces;
 using IbgeApi.Services;
 using IbgeApi.Services.Interfaces;
-using IbgeApi.ValueObjects;
 using IbgeApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IbgeApi.ApiHandlers;
 
@@ -15,34 +15,8 @@ public class AccountHandler
         return service.Signup(model);
     }
 
-    public static async Task<IResult> Login(LoginViewModel model, IUserRepository repository,
-        ITokenService tokenService)
+    public static Task<IResult> Login(IUserService service, LoginViewModel model)
     {
-        model.Validate();
-        if (model.IsValid == false) return Results.BadRequest(model.Notifications);
-
-        try
-        {
-            var user = await repository.GetByEmail(model.Email);
-            var passwordsAreNotEquals = !BCrypt.Net.BCrypt.Verify(model.Password, user.PasswordHash.Hash);
-
-            if (user is null || passwordsAreNotEquals)
-                throw new Exception("Usuário ou senha incorretos");
-
-            var token = tokenService.GenerateToken(user);
-
-            var result = new ResultViewModel<string>()
-            {
-                Message = $"Token gerado para o usuário: {user.Name.FirstName}",
-                Data = token
-            };
-
-            return Results.Ok(result);
-        }
-        catch (Exception e)
-        {
-            var result = new ResultViewModel<string>() { Errors = e.Message };
-            return Results.BadRequest(result);
-        }
+        return service.Login(model);
     }
 }
