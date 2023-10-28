6b515f9e38118fc8c2623fef35ea234efff0a9b6
Achiles <achiles1707@gmail.com>
2023-10-28 11:25:48 -0300

    Fix: GenericRepository methods

--- Repository/Interfaces/IGenericRepository.cs
+++ Repository/Interfaces/IGenericRepository.cs
@@ -1,12 +1,9 @@
-﻿using IbgeApi.ValueObjects;
-
-namespace IbgeApi.Repository.Interfaces;
+﻿namespace IbgeApi.Repository.Interfaces;
 
 public interface IGenericRepository<T> where T : class
 {
-    Task<T> GetByEmail(string email);
-    Task<T> GetByCredentials(string email, string password);
     Task Create(T entity);
+    Task<IList<T>> GetAll(int skip, int take);
     Task Update(T entity);
     Task Remove(T entity);
 }
