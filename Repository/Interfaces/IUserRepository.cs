d5401d45ddf576da78781e280866caae766a5b17
Achiles <achiles1707@gmail.com>
2023-10-28 11:27:00 -0300

    Add: Methods in UserRepository.cs

--- Repository/Interfaces/IUserRepository.cs
+++ Repository/Interfaces/IUserRepository.cs
@@ -4,4 +4,6 @@ namespace IbgeApi.Repository.Interfaces;
 
 public interface IUserRepository : IGenericRepository<UserModel>
 {
+    public Task<UserModel> GetByEmail(string email);
+    public Task<UserModel> GetByCredentials(string email, string password);
 }
